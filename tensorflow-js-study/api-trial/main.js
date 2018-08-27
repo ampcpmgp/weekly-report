cd211b93c5c5cbfffdee1202f1f1dad74e7362d2
Akira Hayatake <akira.hayatake@optim.co.jp>
2018-08-27 13:35:00 +0900

    tensor の example

--- tensorflow-js-study/api-trial/main.js
+++ tensorflow-js-study/api-trial/main.js
@@ -5,7 +5,7 @@ window.tensor = () => {
 
   tf.tensor([1, 2, 3, 4]).print()
   tf.tensor([[1, 2], [3, 4]]).print()
-  tf.tensor([4, 4, 5, 5], [2, 2]).print()
+  tf.tensor([1, 2, 3, 4], [2, 2]).print()
   tf.tensor([[[1], [2]], [[5], [6]]]).print()
 
   tf.tensor1d([1, 2, 3]).print()
@@ -16,6 +16,8 @@ window.tensor = () => {
   tf.tensor5d([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 2, 2, 1]).print()
   tf.tensor6d([1, 2, 3, 4, 5, 6, 7, 8], [1, 1, 2, 2, 2, 1]).print()
 }
+
+// window.tensor()
 
 window.tensorSpecial = () => {
   const buffer = tf.buffer([2, 2])
@@ -38,6 +40,22 @@ window.tensorSpecial = () => {
   image.data[2] = 200
   image.data[3] = 255
   tf.fromPixels(image).print()
+
+  tf.linspace(0, 9, 10).print()
+
+  tf.oneHot(tf.tensor1d([0, 1], 'int32'), 3).print()
+
+  tf.ones([2, 2]).print()
+
+  tf.onesLike(tf.tensor([1, 2])).print()
+
+  tf.range(0, 9, 2).print()
+
+  tf.truncatedNormal([2, 2]).print()
+
+  const x = tf.variable(tf.tensor([1, 2, 3]))
+  x.assign(tf.tensor([4, 5, 6]))
+  x.print()
 }
 
-window.tensorSpecial()
+// window.tensorSpecial()
