196d13017b49dce8839bb0fb1d6e18a6a4b8ea92
ampcpmgp <ampcpmgp@gmail.com>
2018-10-10 14:22:39 +0900

    fpsと状態の表示

--- tensorflow-js-study/9-face-detection/src/app/mixin.js
+++ tensorflow-js-study/9-face-detection/src/app/mixin.js
@@ -2,14 +2,15 @@ import * as cocoSsd from '@tensorflow-models/coco-ssd'
 import sleep from '../util/sleep'
 
 export default {
+  status: '',
   model: null,
-  image: null,
+  image: '',
   result: [],
   isValid: true,
+  fps: 0,
   canvas: document.createElement('canvas'),
 
   async analyze () {
-    console.log('analyze start')
     const context = this.canvas.getContext('2d')
     const width = this.canvas.width
     const height = this.canvas.height
@@ -17,11 +18,17 @@ export default {
     context.drawImage(this.refs.video, 0, 0, width, height)
 
     const imageData = context.getImageData(0, 0, width, height)
+    const now = Date.now()
 
     this.update({
       result: await this.model.detect(imageData),
       image: this.canvas.toDataURL()
     })
+
+    const time = Date.now() - now
+    const ms = 1000
+    const fps = 1 / (time / ms)
+    this.fps = Math.floor(fps * 100) / 100
 
     if (this.isValid) {
       await sleep(0) // 描画させてから実行
@@ -31,7 +38,7 @@ export default {
 
   async init () {
     // mobilenet_v1 or mobilenet_v2 or lite_mobilenet_v2
-    const modelPromise = cocoSsd.load('mobilenet_v2')
+    const modelPromise = cocoSsd.load('lite_mobilenet_v2')
 
     this.on('mount', async () => {
       const videoPromise = navigator.mediaDevices.getUserMedia({ video: true })
@@ -39,7 +46,9 @@ export default {
 
       this.refs.video.srcObject = mediaStream
 
+      this.update({ status: 'model loading...' })
       this.model = await modelPromise
+      this.update({ status: '' })
 
       const width = this.refs.video.videoWidth
       const height = this.refs.video.videoHeight
--- tensorflow-js-study/9-face-detection/src/app/root.tag
+++ tensorflow-js-study/9-face-detection/src/app/root.tag
@@ -1,33 +1,39 @@
 <app-root>
-  <video ref="video" autoplay="true"></video>
-  <section>
-    <img src="{image}">
-    <div
-      class="box"
-      each="{result}"
-      style="
-        left: {bbox[0]}px;
-        top: {bbox[1]}px;
-        width: {bbox[2]}px;
-        height: {bbox[3]}px;
-      "
-      >
-      {class}: {score}
-    </div>
+  <header>
+    <span if="{status}">{status}</span>
+    <span if="{fps}">{fps}fps</span>
+  </header>
+  <section class="capture">
+    <video ref="video" autoplay="true"></video>
+    <section>
+      <img src="{image}">
+      <div
+        class="box"
+        each="{result}"
+        style="
+          left: {bbox[0]}px;
+          top: {bbox[1]}px;
+          width: {bbox[2]}px;
+          height: {bbox[3]}px;
+        "
+        >
+        {class}: {score}
+      </div>
+    </section>
   </section>
 
   <style type="less">
-    :scope {
+    >section.capture {
       display: flex;
-    }
 
-    > section {
-      position: relative;
+      > section {
+        position: relative;
 
-      > .box {
-        color: white;
-        position: absolute;
-        border: solid 1px yellow;
+        > .box {
+          color: white;
+          position: absolute;
+          border: solid 1px yellow;
+        }
       }
     }
   </style>
