074d261de2522732ba99d127ceb39f3a57489eec
ampcpmgp <ampcpmgp@gmail.com>
2018-10-12 15:43:33 +0900

    矩形選択

--- tensorflow-js-study/9-human-detection/src/app/mixin.js
+++ tensorflow-js-study/9-human-detection/src/app/mixin.js
@@ -12,7 +12,116 @@ export default {
   result: [],
   isValid: true,
   fps: 0,
+  selectedX: 0,
+  selectedY: 0,
+  isDraggable: false,
+  isMounseEnter: false,
+  selectedRectangle: {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+    image: ''
+  },
   canvas: document.createElement('canvas'),
+
+  mouseEnter () {
+    this.isMounseEnter = true
+  },
+
+  mouseLeave () {
+    this.isMounseEnter = false
+  },
+
+  startRectangleSelection (e) {
+    const rect = e.target.getBoundingClientRect()
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+
+    this.isDraggable = true
+
+    this.selectedRectangle.x = x
+    this.selectedRectangle.y = y
+    this.selectedRectangle.width = 0
+    this.selectedRectangle.height = 0
+
+    this.update()
+  },
+
+  moveRectanglePoint (e) {
+    if (!this.isDraggable) return
+
+    const rect = e.target.getBoundingClientRect()
+    const x = e.clientX - rect.left
+    const y = e.clientY - rect.top
+
+    const differenceWidth = x - this.selectedRectangle.x
+    const differenceHeight = y - this.selectedRectangle.y
+
+    if (differenceWidth < 0) {
+      this.selectedRectangle.x = x
+      this.selectedRectangle.width += Math.abs(differenceWidth)
+    } else {
+      this.selectedRectangle.width = Math.abs(differenceWidth)
+    }
+
+    if (differenceHeight < 0) {
+      this.selectedRectangle.y = y
+      this.selectedRectangle.height += Math.abs(differenceHeight)
+    } else {
+      this.selectedRectangle.height = Math.abs(differenceHeight)
+    }
+
+    this.update()
+  },
+
+  stopRectangleSelection (e) {
+    this.isDraggable = false
+
+    const imageData = this.drawSelectedRectangle()
+
+    console.log(tf.toPixels(imageData))
+  },
+
+  drawSelectedRectangle () {
+    const { x, y, width, height } = this.selectedRectangle
+    this.refs.selectedRectangleImage.width = width
+    this.refs.selectedRectangleImage.height = height
+
+    const capturedContext = this.refs.captured.getContext('2d')
+    const selectedImageContext = this.refs.selectedRectangleImage.getContext(
+      '2d'
+    )
+    const imageData = capturedContext.getImageData(x, y, width, height)
+
+    selectedImageContext.putImageData(imageData, 0, 0)
+
+    return imageData
+  },
+
+  getLeft (bbox) {
+    const [x] = bbox
+
+    return (x / this.refs.video.videoWidth) * 100
+  },
+
+  getTop (bbox) {
+    const [, y] = bbox
+
+    return (y / this.refs.video.videoHeight) * 100
+  },
+
+  getWidth (bbox) {
+    const [, , width] = bbox
+
+    return (width / this.refs.video.videoWidth) * 100
+  },
+
+  getHeight (bbox) {
+    const [, , , height] = bbox
+
+    return (height / this.refs.video.videoHeight) * 100
+  },
 
   drawCapturedImage (width, height, imageData) {
     const context = this.refs.captured.getContext('2d')
@@ -20,22 +129,29 @@ export default {
     this.refs.captured.height = height
 
     context.putImageData(imageData, 0, 0)
-
-    const imageTensor = tf.fromPixels(imageData)
-
-    // TODO: 転移学習をしたい
-    console.log(imageTensor)
   },
 
-  captureImage (bbox) {
-    const [x, y, width, height] = bbox
+  captureImage () {
     const context = this.canvas.getContext('2d')
-    const imageData = context.getImageData(x, y, width, height)
+    const width = this.refs.video.videoWidth
+    const height = this.refs.video.videoHeight
+    const imageData = context.getImageData(
+      0,
+      0,
+      width,
+      this.refs.video.videoHeight
+    )
 
     this.drawCapturedImage(width, height, imageData)
   },
 
   async analyze () {
+    if (this.isMounseEnter) {
+      await sleep(1000) // 描画させてから実行
+      this.analyze()
+      return
+    }
+
     const context = this.canvas.getContext('2d')
     const width = this.canvas.width
     const height = this.canvas.height
