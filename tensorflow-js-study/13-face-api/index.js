67755356a6c6bbfc91e999567f568b8a2f93bc31
ampcpmgp <ampcpmgp@gmail.com>
2019-01-14 19:55:23 +0900

    検知結果の画像を取得

--- tensorflow-js-study/13-face-api/index.js
+++ tensorflow-js-study/13-face-api/index.js
@@ -1,14 +1,42 @@
 const faceapi = window.faceapi
 const video = window.video
 const results = window.results
+const captures = window.captures
+let lastDetections = []
 
 function sleep (ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+function setCaptures () {
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
+  canvas.width = video.videoWidth
+  canvas.height = video.videoHeight
+  canvas.getContext('2d').drawImage(video, 0, 0)
+
+  captures.innerHTML = ''
+
+  lastDetections.forEach(detection => {
+    const capturedImgCanvas = document.createElement('canvas')
+    const {
+      _box: { _x, _y, _width, _height }
+    } = detection
+
+    const pixel = ctx.getImageData(_x, _y, _width, _height)
+    const capturedImgCtx = capturedImgCanvas.getContext('2d')
+
+    capturedImgCanvas.width = pixel.width
+    capturedImgCanvas.height = pixel.height
+    capturedImgCtx.putImageData(pixel, 0, 0)
+    captures.appendChild(capturedImgCanvas)
+  })
+}
+
 async function setupWebCam () {
   const stream = await navigator.mediaDevices.getUserMedia({ video: true })
   video.srcObject = stream
+  video.onclick = setCaptures
 
   return new Promise(resolve => {
     video.onloadedmetadata = resolve
@@ -20,11 +48,11 @@ async function detect () {
 
   while (true) {
     await sleep(0)
-    const detections = await faceapi.detectAllFaces(video, options)
+    lastDetections = await faceapi.detectAllFaces(video, options)
 
     results.innerHTML = ''
 
-    detections.forEach(detection => {
+    lastDetections.forEach(detection => {
       const {
         _box: { _x, _y, _width, _height }
       } = detection
