35dcd078312dfb2d8a6b39fb7f291437bdce7dd5
ampcpmgp <ampcpmgp@gmail.com>
2019-05-02 22:19:08 +0900

    借用のサンプルを追加

---
+++ rust-study/programming/src/borrowing.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let mut s1 = String::from("hello");
+
+    change(&mut s1);
+
+    let len = calculate_length(&s1);
+
+    println!("{} {} / {}", s1, len, no_dangle());
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn no_dangle() -> String {
+    let s = String::from("hello");
+
+    s // &s だとダングリングポインタのためエラー
+}
