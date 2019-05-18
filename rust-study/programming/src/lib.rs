e2ad05dc8e9fdff4e5a7761b9e85d89b201ffd3a
ampcpmgp <ampcpmgp@gmail.com>
2019-05-18 14:07:06 +0900

    add cargo test structure

--- rust-study/programming/src/lib.rs
+++ rust-study/programming/src/lib.rs
@@ -2,3 +2,4 @@ pub mod adder;
 pub mod greeting;
 pub mod guess;
 pub mod rectangle;
+pub mod test_organization;
---
+++ rust-study/programming/tests/integration_test.rs
@@ -0,0 +1,17 @@
+use study_lib;
+mod common;
+
+#[test]
+fn it_adds_two() {
+    // `cargo test -- --nocapture` を入力することで下記が出力される。
+    println!("{}", "nocapture");
+    assert_eq!(4, study_lib::adder::add_two(2));
+}
+
+// `cargo test -- --ignored` で以下ignore annotationがついたもののみをテスト可能。
+#[ignore]
+#[test]
+fn expensive_test() {
+    common::setup();
+    assert_eq!(4, 4);
+}
