54a925cfa3bd922840d908aebe15655ba1366bd6
ampcpmgp <ampcpmgp@gmail.com>
2019-05-19 19:31:57 +0900

    12-5

--- rust-study/minigrep/src/main.rs
+++ rust-study/minigrep/src/main.rs
@@ -6,12 +6,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("{}", err);
+        eprintln!("{}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("{}", e);
+        eprintln!("{}", e);
 
         process::exit(1);
     }
