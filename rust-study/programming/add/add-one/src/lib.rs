bf1e58021959f64dc640964b0cec9aba22afb851
ampcpmgp <ampcpmgp@gmail.com>
2019-05-22 13:05:39 +0900

    add rust tutorial / 14-3

--- rust-study/programming/add/add-one/src/lib.rs
+++ rust-study/programming/add/add-one/src/lib.rs
@@ -1,7 +1,25 @@
+extern crate rand;
+
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+pub fn random() -> i32 {
+    rand::random()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(3, add_one(2));
+    }
+
+    #[test]
+    fn random_test() {
+        let random = random();
+        assert!(0 < random);
     }
 }
