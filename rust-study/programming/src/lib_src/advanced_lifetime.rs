8dece2dd3a83ad89b0acce743af7a8ba620632f2
ampcpmgp <ampcpmgp@gmail.com>
2019-05-26 23:43:33 +0900

    警告を無くす

--- rust-study/programming/src/lib_src/advanced_lifetime.rs
+++ rust-study/programming/src/lib_src/advanced_lifetime.rs
@@ -1,32 +1,32 @@
 // https://doc.rust-jp.rs/book/second-edition/ch19-02-advanced-lifetimes.html
 
-struct Context<'s>(&'s str);
+pub struct Context<'s>(&'s str);
 
 // struct Parser<'c, 's> { // ドキュメントではこの書き方はエラー
-struct Parser<'c, 's: 'c> {
+pub struct Parser<'c, 's: 'c> {
     context: &'c Context<'s>,
 }
 
 impl<'c, 's> Parser<'c, 's> {
-    fn parse(&self) -> Result<(), &'s str> {
+    pub fn parse(&self) -> Result<(), &'s str> {
         Err(&self.context.0[1..])
     }
 }
 
-fn parse_context(context: Context) -> Result<(), &str> {
+pub fn parse_context(context: Context) -> Result<(), &str> {
     Parser { context: &context }.parse()
 }
 
 // struct Ref<'a, T>(&'a T); // ドキュメントではこの書き方はエラー
 
-struct Ref<'a, T: 'a>(&'a T);
+pub struct Ref<'a, T: 'a>(&'a T);
 
-struct StaticRef<T: 'static>(&'static T);
+pub struct StaticRef<T: 'static>(&'static T);
 
 trait Red {}
 
-struct Ball<'a> {
-    diameter: &'a i32,
+pub struct Ball<'a> {
+    _diameter: &'a i32,
 }
 
 impl<'a> Red for Ball<'a> {}
@@ -34,5 +34,5 @@ impl<'a> Red for Ball<'a> {}
 #[test]
 fn test() {
     let num = 5;
-    let _obj = Box::new(Ball { diameter: &num }) as Box<Red>;
+    let _obj = Box::new(Ball { _diameter: &num }) as Box<Red>;
 }
