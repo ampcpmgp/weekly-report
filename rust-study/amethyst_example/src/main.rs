17d122b620b07b4c9f3fe30bdc15fab477b35b9e
ampcpmgp <ampcpmgp@gmail.com>
2019-11-13 00:36:38 +0900

    WIP

--- rust-study/amethyst_example/src/main.rs
+++ rust-study/amethyst_example/src/main.rs
@@ -1,6 +1,4 @@
 use amethyst::{
-    core::transform::TransformBundle,
-    ecs::prelude::{ReadExpect, Resources, SystemData},
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
@@ -20,22 +18,21 @@ fn main() -> amethyst::Result<()> {
     amethyst::start_logger(Default::default());
 
     let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+    let assets_dir = app_root.join("assets");
+    let mut world = World::new();
 
-    let config_dir = app_root.join("config");
-    let display_config_path = config_dir.join("display.ron");
+    let game_data = GameDataBuilder::default().with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            .with_plugin(RenderFlat2D::default()),
+    )?;
 
-    let game_data = GameDataBuilder::default()
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)
-                        .with_clear([0.34, 0.36, 0.52, 1.0]),
-                )
-                .with_plugin(RenderFlat2D::default()),
-        )?
-        .with_bundle(TransformBundle::new())?;
+    let mut game = Application::new(assets_dir, Pong, game_data)?;
 
-    let mut game = Application::new("/", MyState, game_data)?;
     game.run();
 
     Ok(())
