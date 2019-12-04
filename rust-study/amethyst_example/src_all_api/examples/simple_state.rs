22cd7676a585e51fcf4192717c5f5f1d5a6a2536
ampcpmgp <ampcpmgp@gmail.com>
2019-12-05 00:49:21 +0900

    add simple state

--- rust-study/amethyst_example/src_all_api/examples/simple_state.rs
+++ rust-study/amethyst_example/src_all_api/examples/simple_state.rs
@@ -1,3 +1,24 @@
-pub fn run() {
-    println!("aa");
+use amethyst::prelude::*;
+
+struct GameState;
+
+impl SimpleState for GameState {
+    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
+        println!("Starting Game");
+    }
+
+    fn update(&mut self, _data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        println!("Computing..");
+
+        Trans::Quit
+    }
+}
+
+pub fn run() -> amethyst::Result<()> {
+    let asset_dir = "assets/";
+    let mut game = Application::new(asset_dir, GameState, GameDataBuilder::default())?;
+
+    game.run();
+
+    Ok(())
 }
--- rust-study/amethyst_example/src_all_api/main.rs
+++ rust-study/amethyst_example/src_all_api/main.rs
@@ -1,19 +1,18 @@
 mod examples;
+use amethyst;
 use std::env;
 
-fn main() {
+fn main() -> amethyst::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        println!("`src_all_api/examples` にあるmod名を引数に入れてください");
-
-        return;
+        panic!("`src_all_api/examples` にあるmod名を引数に入れてください");
     }
 
     let name = args[1].as_str();
 
     match name {
         "simple_state" => examples::simple_state::run(),
-        _ => println!("src配下にあるmod名を引数に入れてください"),
+        _ => panic!("`src_all_api/examples` のmod名と一致しません。"),
     }
 }
