a0df82de330a2728c5c55ef97bdb768b1582ebdd
ampcpmgp <ampcpmgp@gmail.com>
2020-11-01 18:55:20 +0900

    js-ipfs 学習環境構築 (#245)

---
+++ js-ipfs-study/index.js
@@ -0,0 +1,29 @@
+import IPFS from 'ipfs'
+
+const nodeP = createNode()
+
+async function createNode() {
+  const node = await IPFS.create({ repo: String(Math.random() + Date.now()) })
+  // generating 2048-bit (rsa only) RSA keypair...
+
+  return node
+}
+
+async function start() {
+  await mappedFunction[location.search]?.()
+}
+
+const mappedFunction = {
+  async ''() {
+    const node = await nodeP
+    const version = await node.version()
+
+    console.log(version)
+  },
+
+  '?BITSWAP'() {
+    console.log('BITSWAP')
+  },
+}
+
+start()
