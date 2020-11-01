0287bd148e4ebf6ca50de86a4e8dcad5ee3814cd
ampcpmgp <ampcpmgp@gmail.com>
2020-11-01 18:35:14 +0900

    prettier 導入

--- js-ipfs-study/index.js
+++ js-ipfs-study/index.js
@@ -1,29 +1,29 @@
-import IPFS from "ipfs";
+import IPFS from 'ipfs'
 
-const nodeP = createNode();
+const nodeP = createNode()
 
 async function createNode() {
-  const node = await IPFS.create({ repo: String(Math.random() + Date.now()) });
+  const node = await IPFS.create({ repo: String(Math.random() + Date.now()) })
   // generating 2048-bit (rsa only) RSA keypair...
 
-  return node;
+  return node
 }
 
 async function start() {
-  await mappedFunction[location.search]?.();
+  await mappedFunction[location.search]?.()
 }
 
 const mappedFunction = {
-  async ""() {
-    const node = await nodeP;
-    const version = await node.version();
+  async ''() {
+    const node = await nodeP
+    const version = await node.version()
 
-    console.log(version);
+    console.log(version)
   },
 
-  "?BITSWAP"() {
-    console.log("BITSWAP");
+  '?BITSWAP'() {
+    console.log('BITSWAP')
   },
-};
+}
 
-start();
+start()
